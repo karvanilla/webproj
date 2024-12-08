e7b227545e0042f19ab07bfcc2e150413c32fef3
karvanilla <158720362+karvanilla@users.noreply.github.com>
2024-12-08 17:24:38 +0300

    Update video.js

--- Karina_files/webitogovy/scripts/video.js
+++ Karina_files/webitogovy/scripts/video.js
@@ -3,54 +3,54 @@ const videos = [
         keyword: "apple_bengali",
         name: "আপেল(apple)",
         category: "Bengali",
-        image: "media/webitogovy/media/Apple.mp4"
+        image: "Karina_files/webitogovy/media/Apple.mp4"
     },
     {
         keyword: "bread_bengali",
         name: "রুটি(bread)",
         category: "Bengali",
-        image: "media/webitogovy/media/bread.mp4"
+        image: "Karina_files/webitogovy/media/bread.mp4"
     },
     {
         keyword: "milk_bengali",
         name: "দুধ(milk)",
         category: "Bengali",
-        image: "media/webitogovy/media/milk.mp4"
+        image: "Karina_files/webitogovy/media/milk.mp4"
     },
     {
         keyword: "zharit_russian",
         name: "Жарить(to fry)",
         category: "Russian",
-        image: "media/webitogovy/media/Жарить (to fry).webm"
+        image: "Karina_files/webitogovy/media/Жарить (to fry).webm"
     },
     {
         keyword: "znat_russian",
         name: "Знать(to know)",
         category: "Russian",
-        image: "media/webitogovy/media/Знать (to know).webm"
+        image: "Karina_files/webitogovy/media/Знать (to know).webm"
     },
     {
         keyword: "idti_russian",
         name: "Идти(to go)",
         category: "Russian",
-        image: "media/webitogovy/media/Идти (to go or to walk).webm"
+        image: "Karina_files/webitogovy/media/Идти (to go or to walk).webm"
     },
     {
         keyword: "day_american",
         name: "day",
         category: "American",
-        image: "media/webitogovy/media/Day.mp4"
+        image: "Karina_files/webitogovy/media/Day.mp4"
     },
     {
         keyword: "year_american",
         name: "year",
         category: "American",
-        image: "media/webitogovy/media/year.mp4"
+        image: "Karina_files/webitogovy/media/year.mp4"
     },
     {
         keyword: "week_american",
         name: "week",
         category: "American",
-        image: "media/webitogovy/media/week.mp4"
+        image: "Karina_files/webitogovy/media/week.mp4"
     }
 ];
