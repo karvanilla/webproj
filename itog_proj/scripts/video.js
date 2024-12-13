10d86d5500222caba839bc9f312ec8ec92535d00
isohise <158493027+isohise@users.noreply.github.com>
2024-12-13 18:56:05 +0300

    Add files via upload

---
+++ itog_proj/scripts/video.js
@@ -0,0 +1,56 @@
+const videos = [
+    {
+        keyword: "apple_bengali",
+        name: "আপেল (apple)",
+        category: "Bengali",
+        image: "media/Bengali/Apple.mp4"
+    },
+    {
+        keyword: "bread_bengali",
+        name: "রুটি (bread)",
+        category: "Bengali",
+        image: "media/Bengali/bread.mp4"
+    },
+    {
+        keyword: "milk_bengali",
+        name: "দুধ (milk)",
+        category: "Bengali",
+        image: "media/Bengali/milk.mp4"
+    },
+    {
+        keyword: "zharit_russian",
+        name: "Жарить (to fry)",
+        category: "Russian",
+        image: "media/Russian/Жарить (to fry).webm"
+    },
+    {
+        keyword: "znat_russian",
+        name: "Знать (to know)",
+        category: "Russian",
+        image: "media/Russian/Знать (to know).webm"
+    },
+    {
+        keyword: "idti_russian",
+        name: "Идти (to go)",
+        category: "Russian",
+        image: "media/Russian/Идти (to go or to walk).webm"
+    },
+    {
+        keyword: "day_american",
+        name: "day",
+        category: "American",
+        image: "media/American/Day.mp4"
+    },
+    {
+        keyword: "year_american",
+        name: "year",
+        category: "American",
+        image: "media/American/year.mp4"
+    },
+    {
+        keyword: "week_american",
+        name: "week",
+        category: "American",
+        image: "media/American/week.mp4"
+    }
+];
